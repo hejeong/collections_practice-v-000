cf0cb57b46bb168b6b954b4b5412a817b1f74076
Jonathan Hong <jonhong777@gmail.com>
2018-07-05 01:50:12 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,7 +3,15 @@ def sort_array_asc(int_array)
 end
 
 def sort_array_desc(int_array)
-  
+  int_array.sort do |a , b|
+    if a == b 
+      0
+    elsif a < b
+      1 
+    elsif a > b 
+      -1
+    end
+  end
 end
 
 def sort_array_char_count(str_array)
